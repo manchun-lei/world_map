b3f6750848c66430d12dacde5ce84f974fef3a38
Manchun LEI <59614357+manchun-lei@users.noreply.github.com>
2025-06-06 11:55:44 +0200

    add comment

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@
 from word_map import *
 
 area = [-5, 41, 10, 52] # [lon0,lat0,lon1,lat1]
-sub_area = [1.7, 48.5, 2.8, 49]
+sub_area = [1.7, 48.5, 2.8, 49] # or = None 
 fig,ax = word_map_lonlat(area, sub_area=sub_area, dstfile=None, figure_height=8, fontsize=14)
 add_scalebar_x(ax, (0.7,0.05), area, 100, color="black",fontsize=10)
 plt.savefig('demo.png')
